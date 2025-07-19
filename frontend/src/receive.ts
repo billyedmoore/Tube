29eb21e4d2329b8fea4f68065810e3dc7c2c980e
Billy Moore <billyedmoore@yahoo.com>
2025-07-19 19:52:27 +0100

    Created next.js app and added favicon.

--- frontend/src/receive.ts
+++
@@ -1,8 +0,0 @@
-export function handleRecieveFormComplete(event: SubmitEvent): void {
-	event.preventDefault();
-
-	const fileName = (document.getElementById('fileName') as HTMLInputElement).value;
-	const sendID = (document.getElementById('sendID') as HTMLInputElement).value;
-
-	console.log(`Saving the share ${sendID} to ${fileName}.`)
-}
